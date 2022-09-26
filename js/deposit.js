15ad4d61c6e4ff9463660d164e9c5620cff47013
mahmudulhasan22 <mahmdulhasan001@gmail.com>
2022-09-27 00:12:26 +0600

    first commit

---
+++ js/deposit.js
@@ -0,0 +1,18 @@
+document.getElementById('btn-deposit').addEventListener('click',function(){
+    const newDepositAmmount = getInputFieldvalueById('deposit-field');
+    const prevDepositTotal = getTextElementValueById('deposit-total');
+
+    const newDepositTotal = prevDepositTotal + newDepositAmmount ;
+    setTextElementValueByID('deposit-total' , newDepositTotal);
+
+    const prevBalanceTotal = getTextElementValueById('main-balance');
+    const newBalanceTotal = prevBalanceTotal + newDepositAmmount ;
+    setTextElementValueByID('main-balance' , newBalanceTotal);
+})
+
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    const newWithdrawAmmount = getInputFieldvalueById('withdraw-field');
+    const prevWithdrawTotal = getTextElementValueById('withdraw-total');
+    const newWithdrawTotal = prevWithdrawTotal + newWithdrawAmmount;
+    setTextElementValueByID('withdraw-total', newWithdrawTotal);
+})
---
+++ js/utility.js
@@ -0,0 +1,18 @@
+function getInputFieldvalueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value ;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue ;
+}
+
+function getTextElementValueById(elementId){
+    const textElement = document.getElementById(elementId);
+    const textElementValueString = textElement.innerText;
+    const textElementValue = parseFloat(textElementValueString);
+    return textElementValue;
+}
+function setTextElementValueByID(elementId , newValue){
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue ;
+}
